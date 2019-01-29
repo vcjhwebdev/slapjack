006d93e4e01d310c1c802a791e025ab2586051a6
Tony Zhou <tony.zhou@warriorlife.net>
2019-01-29 09:36:35 -0800

    shuffle deck code 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,10 @@ deck.addEventListener('click', function(e) {
   // e.target <-- thing you clicked on
   // e.target.className <-- class of the thing
   // e.target.style.display = "none" <-- hides the thing
-  alert('You clicked on the deck');
+  var nextCard = deckOfCards.pop();
+  if(nextCard !== undefined) {
+    cardInPlay.src = "img/cards/1x/" + nextCard + ".png";
+  }
 });
 
 cardInPlay.addEventListener('click', function(e) {
@@ -34,7 +37,7 @@ function shuffle(array) {
   return array;
 }
 var cardsArr = ["club_jack", "club_queen", "club_king", "diamond_jack", "diamond_queen", "diamond_king", "heart_jack", "heart_queen", "heart_king", "spade_jack", "spade_queen", "spade_king"];
-for (i=0; i<10; i++) {
+for (i=1; i<=10; i++) {
   cardsArr.push("club_" + i);
   cardsArr.push("diamond_" + i);
   cardsArr.push("heart_" + i);
