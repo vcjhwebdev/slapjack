28e77bf242879a84486e41edb5fb5c0b2be50d12
Tony Zhou <tony.zhou@warriorlife.net>
2019-01-29 09:16:55 -0800

    shuffle deck code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var deckOfDards = [];
+var deckOfCards = [];
 var deck = document.querySelector('.deck');
 var cardInPlay = document.querySelector('.play');
 
@@ -15,6 +15,30 @@ cardInPlay.addEventListener('click', function(e) {
   alert('You clicked on a card in play. It\'s file is ' + src);
 });
 
-function shuffle() {
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
+var cardsArr = ["club_jack", "club_queen", "club_king", "diamond_jack", "diamond_queen", "diamond_king", "heart_jack", "heart_queen", "heart_king", "spade_jack", "spade_queen", "spade_king"];
+for (i=0; i<10; i++) {
+  cardsArr.push("club_" + i);
+  cardsArr.push("diamond_" + i);
+  cardsArr.push("heart_" + i);
+  cardsArr.push("spade_" + i);
+}
+deckOfCards = shuffle(cardsArr);
+//console.log(cardsArr);
