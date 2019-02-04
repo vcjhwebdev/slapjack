06521bb7142e85aa79fcd8331c100bb81930343f
Tony Zhou <tony.zhou@warriorlife.net>
2019-02-04 09:42:55 -0800

    function shuffle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
-var deckOfCards = [];
 var deck = document.querySelector('.deck');
 var cardInPlay = document.querySelector('.play');
+var computerHand = [];
+var playerHand = [];
 
 deck.addEventListener('click', function(e) {
-  var nextCard = deckOfCards.pop();
+  var nextCard = playerHand.pop();
   if(nextCard !== undefined) {
     cardInPlay.src = "img/cards/2x/" + nextCard + ".png";
   }
@@ -14,31 +15,43 @@ cardInPlay.addEventListener('click', function(e) {
   alert('You clicked on a card in play. It\'s file is ' + src);
 });
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+// shuffle and deal
+function shuffle() {
+
+  var allPossibleCards = ["club_jack", "club_queen", "club_king", "diamond_jack", "diamond_queen", "diamond_king", "heart_jack", "heart_queen", "heart_king", "spade_jack", "spade_queen", "spade_king"];
+  for (i=1; i<=10; i++) {
+    allPossibleCards.push("club_" + i);
+    allPossibleCards.push("diamond_" + i);
+    allPossibleCards.push("heart_" + i);
+    allPossibleCards.push("spade_" + i);
+  }
+
+  function deal(deck) {
+    // deal the cards
+      for (i=0; i<26; i++) {
+        computerHand.push(deck.pop());
+      }
+    //console.log(computerArray);
+
+    var playerArray = []
+      for (i=0; i<26; i++) {
+        playerHand.push(deck.pop());
+      }
+      //console.log(playerArray);
+  }
+
+  var currentIndex = allPossibleCards.length, temporaryValue, randomIndex;
   while (0 !== currentIndex) {
 
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+    temporaryValue = allPossibleCards[currentIndex];
+    allPossibleCards[currentIndex] = allPossibleCards[randomIndex];
+    allPossibleCards[randomIndex] = temporaryValue;
   }
 
-  return array;
+  deal(allPossibleCards);
 }
-var cardsArr = ["club_jack", "club_queen", "club_king", "diamond_jack", "diamond_queen", "diamond_king", "heart_jack", "heart_queen", "heart_king", "spade_jack", "spade_queen", "spade_king"];
-for (i=1; i<=10; i++) {
-  cardsArr.push("club_" + i);
-  cardsArr.push("diamond_" + i);
-  cardsArr.push("heart_" + i);
-  cardsArr.push("spade_" + i);
-}
-deckOfCards = shuffle(cardsArr);
 
-var computerArray = []
-  for (i=0; i<26; i++) {
-    computerArray.push(cardsArr[i])
-  }
-console.log(computerArray);
+shuffle();
